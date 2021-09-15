b8ef4bc16a3e7722dcd7cb65c8bf8bf3a1dc59a4
cv <coopervick98@gmail.com>
2021-09-15 09:07:38 -0500

    Replaced Employee Controller and Service with Login Controller and Service

---
+++ src/main/java/com/cpo/bank/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.cpo.bank.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	@PostMapping("/login")
+	private void employeeLogin(@RequestBody Map<String, Object> request) {
+		
+	}
+
+}
---
+++ src/main/java/com/cpo/bank/service/LoginService.java
@@ -0,0 +1,14 @@
+package com.cpo.bank.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cpo.bank.repository.EmployeeRepository;
+
+@Service
+public class LoginService {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+}
