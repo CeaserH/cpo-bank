e237a58cd10e0ae98bfd5d4937a0c67ffabc1110
cv <coopervick98@gmail.com>
2021-09-15 08:30:05 -0500

    Fixed Methods in Account Controller to match user stories

--- src/main/java/com/cpo/bank/controller/AccountController.java
+++ src/main/java/com/cpo/bank/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.cpo.bank.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,36 +14,47 @@ public class AccountController {
 
 	@Autowired
 	AccountService accountService;
-	
+
 	//get All Accounts
-	@GetMapping("/allAccounts")
+	@GetMapping("/accounts/all")
 	private List<Account> getAllAccounts(){
 		return accountService.getAllAccounts();
 	}
 	
 	// get Account by ID
-	@GetMapping("/account/{accountID}")
+	@GetMapping("/accounts/id/{accountID}")
 	private Account getAccount(@PathVariable("accountID")long accountID) {
 		return accountService.getAccountById(accountID);
 	}
 	
-	//add Account
-	@PostMapping("/account")
-	private Account saveAccount(@RequestBody Account account) {
-		accountService.createAccount(account);
-		return account;
+	//Account Creation
+	@PostMapping("/accounts/create")
+	private void createAccount(@RequestBody Map<String, Object> request) {
+		//accountService.createAccount(account);
 	}
 	
-	//update Account
-	@PutMapping("/account")
-	private Account update(@RequestBody Account account) {
-		accountService.updateAccount(account);
-		return account;
+	//Update Customer Name
+	@PutMapping("/accounts/id/{accountID}/name/{customerName}")
+	private void updateCustomerName(@PathVariable("accountID") long accountID, @PathVariable("customerName") String customerName) {
+		
+	}
+	
+	//Update Customer Contact
+	@PutMapping("/accounts/id/{accountID}/contact/{customerContact}")
+	private void updateCustomerContact(@PathVariable("accountID") long accountID, @PathVariable("customerContact") String customerContact) {
+		
+	}
+	
+	//Update Customer Address
+	@PutMapping("/accounts/update/address")
+	private void updateCustomerContact(@RequestBody Map<String, Object> request) {
+		
 	}
 	
 	//Delete Account
-	@DeleteMapping("/account/{accountID}")
+	@DeleteMapping("/accounts/delete/{accountID}")
 	private void deleteAccount(@PathVariable("accountID") int accountID) {
 		accountService.deleteById(accountID);
 	}
+	
 }
