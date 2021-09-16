3a7e925d2b0f24bb85908c2da006657a34989aea
cv <coopervick98@gmail.com>
2021-09-16 13:28:15 -0500

    Finished cleaning model classes, hopefully fixed gitignore

--- src/main/java/com/cpo/bank/model/Passbook.java
+++ src/main/java/com/cpo/bank/model/Passbook.java
@@ -8,56 +8,36 @@ public class Passbook {
 
 	@Id
 	@Column
-	private int id;
-	private long accountID;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long passbookID;
+	
+	@OneToOne
+	@JoinColumn(name="accountID", nullable=false)
+	private Account account;
 	
 	@ManyToOne
 	@JoinColumn(name="transactionID", nullable=false)
-	private Transaction transaction;
+	private Transaction transaction; //I think this should be a set of transactions
 	
-	
-	public Passbook() {
-		
+	public Passbook() {}
+
+	public long getPassbookID() {
+		return passbookID;
 	}
-
-
-	public Passbook(int id, long accountID, Transaction transaction) {
-		super();
-		this.id = id;
-		this.accountID = accountID;
-		this.transaction = transaction;
+	public void setPassbookID(long passbookID) {
+		this.passbookID = passbookID;
 	}
-
-
-	public int getId() {
-		return id;
+	public Account getAccount() {
+		return account;
 	}
-
-
-	public void setId(int id) {
-		this.id = id;
+	public void setAccount(Account account) {
+		this.account = account;
 	}
-
-
-	public long getAccountID() {
-		return accountID;
-	}
-
-
-	public void setAccountID(long accountID) {
-		this.accountID = accountID;
-	}
-
-
 	public Transaction getTransaction() {
 		return transaction;
 	}
-
-
 	public void setTransaction(Transaction transaction) {
 		this.transaction = transaction;
 	}
-	
-	
 	
 }
