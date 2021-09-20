26adbff8ebab8d11789c962ce93ed8edd4f0e9c8
cv <coopervick98@gmail.com>
2021-09-20 14:27:29 -0500

    Added slip functionality to transaction service

--- src/main/java/com/cpo/bank/service/TransactionService.java
+++ src/main/java/com/cpo/bank/service/TransactionService.java
@@ -6,8 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cpo.bank.model.Account;
 import com.cpo.bank.model.Check;
 import com.cpo.bank.model.Transaction;
+import com.cpo.bank.repository.AccountRepository;
 import com.cpo.bank.repository.TransactionRepository;
 
 @Service
@@ -15,14 +17,40 @@ public class TransactionService {
 
 	@Autowired
 	TransactionRepository transactionRepository;
+	@Autowired
+	AccountRepository accountRepository;
 	
 	//Credit Via Slip
 	public void slipCredit(long id, double amount) {
+		
+		Transaction transact = new Transaction();
+		
+		transact.setTransactionMethod("Slip");
+		transact.setTransactionType("Credit");
+		transact.setAmount(amount);
+		
+		Account account = accountRepository.findById(id).get();
+		transact.setAccount(account);
+		account.setAccountBalance(account.getAccountBalance() + amount);
+		
+		transactionRepository.save(transact);
 		
 	}
 	
 	//Debit Via Slip
 	public void slipDebit(long id, double amount) {
+		
+Transaction transact = new Transaction();
+		
+		transact.setTransactionMethod("Slip");
+		transact.setTransactionType("Debit");
+		transact.setAmount(amount);
+		
+		Account account = accountRepository.findById(id).get();
+		transact.setAccount(account);
+		account.setAccountBalance(account.getAccountBalance() - amount);
+		
+		transactionRepository.save(transact);
 		
 	}
 	
